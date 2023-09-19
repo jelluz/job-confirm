4efdf81f1a2454e3af9217020d0489f800e93ae4
Jelle <jelle@mx3d.com>
2023-09-19 17:10:43 +0200

    working with starlette-admin

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from typing import Optional
 from sqlmodel import Field, Session, SQLModel, create_engine, select
-from sqladmin import Admin, ModelView
+# from sqladmin import Admin, ModelView
+from starlette_admin.contrib.sqlmodel import Admin, ModelView
+
+from starlette_admin import (
+    CollectionField,
+    ColorField,
+    EmailField,
+    ExportType,
+    IntegerField,
+    JSONField,
+    ListField,
+    StringField,
+    URLField,
+)
+from sqlalchemy import Column, Text
+
 from fastapi import FastAPI
 
 
@@ -8,10 +23,11 @@ class Outage(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str = Field(index=True)
     description: str = Field(max_length=2000)
+    content: str = Field(sa_column=Column(Text))
 
 
-class OutageAdmin(ModelView, model=Outage):
-    column_list = [Outage.id, Outage.title, Outage.description]
+# class OutageAdmin(ModelView, model=Outage):
+#     column_list = [Outage.id, Outage.title, Outage.description]
 
 
 
@@ -26,9 +42,18 @@ def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
+class MyModelView(ModelView):
+    pass
+    # page_size = 5
+    # page_size_options = [5, 10, 25 - 1]
+    # export_types = [ExportType.EXCEL, ExportType.CSV]
+
+
 app = FastAPI()
-admin = Admin(app, engine)
-admin.add_view(OutageAdmin)
+admin = Admin(engine)
+admin.add_view(MyModelView(Outage))
+# admin.add_view(OutageAdmin)
+admin.mount_to(app)
 
 
 @app.on_event("startup")
